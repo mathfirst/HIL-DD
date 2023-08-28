d8cf8aa46a2561788f9010adaa5f6925d4b8b359
math-first <kkai_zhao@yeah.net>
2023-08-28 10:02:41 +0800

    debug=False

--- HIL_DD_ui_learning.py
+++ HIL_DD_ui_learning.py
@@ -240,7 +240,7 @@ if __name__ == '__main__':
     t0 = time.time()
     for num_inj in range(10000):
         if time.time() - t0 > 10*60:
-            logger.info(f"You have made no operations during the past 30 minutes. Exiting...")
+            logger.info(f"You have made no operations during the past 10 minutes. Exiting...")
             sys.exit()
         while True:
             get_proposals(pt_dir, proposal_base_dict, logger=logger.info)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e@l5*12o@10!q$2@f)=(%mn6w940whkmxcxy)(9l-y^d7_l0co"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # True
+DEBUG = False  # True
 
 ALLOWED_HOSTS = ['118.193.102.41', '118.193.102.42', '118.193.102.43', '118.193.102.35', '118.193.102.36', '118.193.102.38', 'localhost', '127.0.0.1']
 
