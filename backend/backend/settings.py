2fabf43469c80d532b0df67c9f4980b83bbe885d
mathfirst <kkai_zhao@yeah.net>
2023-08-31 10:43:28 +0800

    CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
