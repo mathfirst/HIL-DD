a369b3eb6891390032655f22c3426b28e0ff4e0a
mathfirst <kkai_zhao@yeah.net>
2023-09-04 16:54:25 +0800

    add logger for views

--- backend/app01/views.py
+++ backend/app01/views.py
@@ -13,7 +13,32 @@ from sys import platform
 
 # sys.path.append('../../../HIL-DD')
 # Create your views here.
+def get_logger(name, log_filename=None):
+    import logging
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter('[%(asctime)s::%(name)s::%(levelname)s] %(message)s')
+    # formatter = logging.Formatter('[%(asctime)s-%(name)s] %(message)s')
+    formatter = logging.Formatter('[%(asctime)s] %(message)s')
 
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(formatter)
+    #     logger.addHandler(stream_handler)
+    #     https://alexandra-zaharia.github.io/posts/fix-python-logger-printing-same-entry-multiple-times/
+    if not logger.hasHandlers():  # check whether any handlers are already attached before adding them to the logger
+        logger.addHandler(stream_handler)
+        # logger.addHandler(file_handler)
+    if log_filename is not None:
+        file_handler = logging.FileHandler(log_filename)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+logger = get_logger('ui_log', 'log_ui.txt')
 
 def test(request):
     return HttpResponse('pref')
@@ -24,7 +49,7 @@ def getPDBList(request):
     df = pd.read_csv("../configs/PDB_ID_CrossDocked_testset.csv", encoding="utf-8")
     full_pdb_dir = 'app01/static/full_pdb/'
     if not os.path.isdir(full_pdb_dir):
-        print(f'making dir {full_pdb_dir}')
+        logger.info(f'making dir {full_pdb_dir}')
         os.makedirs(full_pdb_dir)
     pdb_list = []
     for i, j in zip(df['Index'], df['PDB_ID']):
@@ -55,15 +80,14 @@ def getAnnotations(request):
 def getTimePDB(request):
     timestamp = str(request.POST.get('timestamp'))
     pdb = str(request.POST.get('pdb_id'))
-    print(timestamp)
-    print(pdb)
-    print('starting a human-in-the-loop drug design program which is driven by preference learning')
+    logger.info(f"In getTimePDB, {timestamp}, {pdb}")
+    logger.info('In getTimePDB, starting a human-in-the-loop drug design program which is driven by preference learning')
     output = os.getcwd()
-    print('current dir', output)
+    logger.info(f'current dir {output}')
     if not output.endswith('HIL-DD'):
         os.chdir('../')
         output = os.getcwd()
-        print('after changing dir', output)
+        logger.info(f'after changing dir {output}')
     Popen(['python3', 'HIL_DD_ui_proposals.py', timestamp, pdb, '--device cuda:1'], shell=False,
           close_fds=True)  # , creationflags=DETACHED_PROCESS)
     time.sleep(30)
@@ -75,7 +99,7 @@ def getTimePDB(request):
     output = os.getcwd()
     if output.strip().endswith('HIL-DD'):
         os.chdir('./backend/')
-        print('changed back to', os.getcwd())
+        logger.info(f'changed back to {os.getcwd()}')
 
     # return redirect(f"/api/getMoleculeList/?timestamp={logdir}&pdb={pdb}")
     return sendMoleculeList(request, True)
@@ -84,13 +108,13 @@ def getTimePDB(request):
 def sendMoleculeList(request, start=False):
     timestamp = str(request.POST.get('timestamp'))
     if start:
-        print('starting a task...')
+        logger.info('sendMoleculeList, starting a task...')
     else:
-        print('getting annotations...')
+        logger.info('sendMoleculeList, getting annotations...')
         # like_ids = request.POST.get('liked_ids')
         # dislike_ids = request.POST.get('disliked_ids')
         received_json_data = json.loads(request.body)
-        print(received_json_data)
+        logger.info(f"sendMoleculeList, received_json_data: {received_json_data}")
         timestamp = received_json_data['timestamp']
         # pdb_id = received_json_data.pdb_id
         like_ids = received_json_data['liked_ids']
@@ -102,16 +126,15 @@ def sendMoleculeList(request, start=False):
         with open(annotation_path, "w") as outfile:
             json.dump(annotation_dict, outfile, indent=2)
     pdb = str(request.POST.get('pdb_id'))
-    print('sendMoleculeList', timestamp)
-    print(pdb)
+    logger.info(f'sendMoleculeList: {timestamp}, {pdb}')
     proposal_dir = os.path.join('./app01/static/', timestamp, 'proposals')
     proposal_json = os.path.join(proposal_dir, 'proposals.json')
-    print(os.path.abspath(proposal_json))
+    logger.info(f"sendMoleculeList: {os.path.abspath(proposal_json)}")
     while True:
         if os.path.isfile(proposal_json):
-            print(f"loading {proposal_json}")
-            os.system('django-admin collectstatic')
-            print('django-admin collectstatic is done.')
+            logger.info(f"loading {proposal_json}")
+            # os.system('django-admin collectstatic')
+            # print('django-admin collectstatic is done.')
             time.sleep(0.1)
             with open(proposal_json, 'r') as f:
                 proposals = json.load(f)
@@ -147,13 +170,12 @@ def sendMoleculeList(request, start=False):
 def evaluation(request):
     timestamp = str(request.GET.get('timestamp'))
     pdb = str(request.GET.get('pdb'))
-    print(timestamp)
-    print(pdb)
+    logger.info(f"evaluation: {timestamp}, {pdb}")
     eval_dir = os.path.join('./app01/static/', timestamp, 'evaluation')
     evaluation_json = os.path.join(eval_dir, 'evaluation.json')
     while True:
         if os.path.isfile(evaluation_json):
-            print(f"loading {evaluation_json}")
+            logger.info(f"loading {evaluation_json}")
             time.sleep(0.1)
             with open(evaluation_json, 'r') as f:
                 evaluation_samples = json.load(f)
